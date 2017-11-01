0a8732a0f70e513e0deb9d49819f935fff2be4e4
unknown <samarthagarwal@live.com>
2017-11-01 23:16:41 +0530

    Final

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,21 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  weatherInfo: any = {};
 
+  constructor(public navCtrl: NavController, public http: Http) {
+
+    this.getData();
+    
+  }
+
+  getData(){
+    this.http.get("http://api.openweathermap.org/data/2.5/weather?q=london&appId=53591a412c95932221df665561b01151").subscribe((data) => {
+      console.log(data.json())
+      this.weatherInfo = data.json();
+    }, (error) => {
+      console.log(error)
+    })
   }
 
 }

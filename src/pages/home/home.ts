ead4a265c3bd103b6fd30b3d8fd3cc7f7af9e0bb
unknown <samarthagarwal@live.com>
2017-11-02 00:21:08 +0530

    Final

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController } from 'ionic-angular';
 import { Http } from '@angular/http';
 
 @Component({
@@ -9,19 +9,24 @@ import { Http } from '@angular/http';
 export class HomePage {
 
   weatherInfo: any = {};
+  cityName: string = "London";
 
-  constructor(public navCtrl: NavController, public http: Http) {
+  constructor(public navCtrl: NavController, public http: Http, public loadingCtrl: LoadingController) {
 
     this.getData();
     
   }
 
   getData(){
-    this.http.get("http://api.openweathermap.org/data/2.5/weather?q=london&appId=53591a412c95932221df665561b01151").subscribe((data) => {
+    let loading = this.loadingCtrl.create();
+    loading.present();
+    this.http.get("http://api.openweathermap.org/data/2.5/weather?q=" + this.cityName + "&appId=53591a412c95932221df665561b01151").subscribe((data) => {
       console.log(data.json())
       this.weatherInfo = data.json();
+      loading.dismiss();
     }, (error) => {
-      console.log(error)
+      console.log(error);
+      loading.dismiss();
     })
   }
 
